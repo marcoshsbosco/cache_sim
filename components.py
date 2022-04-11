51d58fc1b25d08d6203cf8a7c1e3a5c8dc0e3d4b
eletricmano <58055884+eletricmano@users.noreply.github.com>
2022-04-11 18:10:39 -0300

    Fixed mapping algorithm running with free lines in cache

--- components.py
+++ components.py
@@ -14,10 +14,23 @@ class CPU:
 
             blk_addr, block = self.ram.read(addr)
 
-            if self.replacement == "random":
-                line = random.randint(0, self.cache.size // self.cache.line_size - 1)
+            if self.cache.free_line == None:
+                print("No free lines in cache")
+
+                if self.replacement == "random":
+                    print("Using random replacement algorithm")
+                    line = random.randint(0, self.cache.size // self.cache.line_size - 1)
+            else:
+                print("Free line found in cache")
+                line = self.cache.free_line
 
             self.cache.write(block, blk_addr, line)
+            if self.cache.free_line != None:
+                if self.cache.free_line == self.cache.size // self.cache.line_size - 1:
+                    self.cache.free_line = None
+                else:
+                    self.cache.free_line += 1
+
             word = self.cache.read(addr)
         else:
             print("Hit")
@@ -54,6 +67,7 @@ class Cache:
         self.line_size = line_size
         self.lines = []  # actual memory space
         self.tags = []  # RAM addresses
+        self.free_line = 0
 
         # initializes empty cache tags and lines
         for i in range(size // line_size):
@@ -65,10 +79,12 @@ class Cache:
         blk_addr = blk * self.line_size
 
         if blk_addr in self.tags:  # hit
+            print(f"Reading from line {self.tags.index(blk_addr)}")
             return self.lines[self.tags.index(blk_addr)][addr % self.line_size]  # returns word
         else:  # miss
             return None
 
     def write(self, data: list, tag: int, line: int):
+        print(f"Writing to line {line}")
         self.tags[line] = tag
         self.lines[line] = data
