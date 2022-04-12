07f322caa893b8ada0a2f78d8c4f2fe7108d0452
eletricmano <58055884+eletricmano@users.noreply.github.com>
2022-04-11 21:02:59 -0300

    Implemented random access generator

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import components
+import random
+
+def random_access(quantity: int):
+    for i in range(quantity):
+        addr = random.randint(0, len(ram.memory) - 1)
+        print(f"-----Requesting read at address {addr}-----")
+        print(f"Word: {cpu.read(addr)}\n")
 
 ram = components.RAM(size=1024, blk_size=2)
 cache = components.Cache(size=16, line_size=2)
 cpu = components.CPU(cache, ram, replacement="random")
 
-print(f"Word: {cpu.read(0)}\n")
-print(f"Word: {cpu.read(1)}\n")
-print(f"Word: {cpu.read(2)}\n")
+random_access(quantity=32)
