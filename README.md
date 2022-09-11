b300fda21b831539bc40c769ef6954081990c75a
marcoshsbosco <58055884+marcoshsbosco@users.noreply.github.com>
2022-09-11 17:58:46 -0300

    Added compatibility for multiple CPUs with their respective caches Added manual accesses to memory with user-defined addresses

--- README.md
+++ README.md
@@ -1,21 +1,6 @@
 # Cache memory simulator
-This program simulates how cache memory works and interacts with the processor and main memory of a computer.
+This program simulates how cache memory works and interacts with the processor and main memory of a computer using the MESI protocol.
 
 ## Instructions
 ### Running the program
 Open a terminal in the main directory, type `make` and press Enter.
-
-### Component parameters
-Lines 16 through 18 of `main.py` contain the constructors for the components to be simulated. There, you can change RAM memory and block size, cache memory and line size, and CPU replacement algorithm (`lfu` or `random`).
-At line 20, you may change the quantity of random accesses generated and sent to the CPU.
-
-### Manual memory accesses
-Instead of random accesses using the `random_access` function, you may opt to manually issue commands to the CPU.
-
-- `cpu.read(addr: int)`
-
-Retrieves the word located at address `addr` of main memory. Inserting this method in a `print` call prints the word to the terminal.
-
-- `cpu.read_modify(new_data: int, addr: int)`
-
-Retrieves the word located at address `addr` of main memory. Inserting this method in a `print` call prints the word to the terminal. After reading the word from memory, this function also overwrites `new_data` to address `addr` of main memory.
--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 import components
 import random
 
-def random_access(quantity: int):
-    for i in range(quantity):
-        access = random.choice(["read", "read_modify"])
-        addr = random.randint(0, len(ram.memory) - 1)
-        print(f"-----Requesting {access} at address {addr}-----")
-
-        if access == "read":
-            print(f"Word: {cpu.read(addr)}\n")
-        else:
-            new_data = random.randint(0, 255)
-            print(f"Word: {cpu.read_modify(new_data, addr)}\n")
-
 ram = components.RAM(size=1024, blk_size=2)
-cache = components.Cache(size=16, line_size=2)
-cpu = components.CPU(cache, ram, replacement="lfu")
+cache1 = components.Cache(size=16, line_size=2)
+cache2 = components.Cache(size=16, line_size=2)
+cache3 = components.Cache(size=16, line_size=2)
+cache4 = components.Cache(size=16, line_size=2)
+cpu1 = components.CPU(cache1, ram, replacement="lfu")
+cpu2 = components.CPU(cache2, ram, replacement="lfu")
+cpu3 = components.CPU(cache3, ram, replacement="lfu")
+cpu4 = components.CPU(cache4, ram, replacement="lfu")
 
-random_access(quantity=32)
+def access(cpu, acc_type, addr):
+    print(f"\n-----Requesting {acc_type} at address {addr}-----")
+
+    if acc_type == "read":
+        print(f"Word: {cpu.read(addr)}\n")
+    else:
+        new_data = random.randint(0, 255)
+        print(f"Word: {cpu.read_modify(new_data, addr)}\n")
+
+def menu():
+    print("-----Choose a processor-----")
+    print("1. CPU 1")
+    print("2. CPU 2")
+    print("3. CPU 3")
+    print("4. CPU 4")
+    print("0. Exit program")
+
+    cpu = input("")
+    if cpu == "0":
+        exit()
+
+    print("\n-----Choose an access type-----")
+    print("1. Read")
+    print("2. Modify")
+
+    acc_type = "read" if input("") == "1" else "read_modify"
+
+    print(f"\n-----Type an address (0 to {len(ram.memory) - 1})-----")
+    addr = int(input(""))
+
+    access(eval(f"cpu{cpu}"), acc_type, addr)
+
+while True:
+    menu()
